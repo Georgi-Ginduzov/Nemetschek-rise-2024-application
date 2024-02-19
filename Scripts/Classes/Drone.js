07482a465067201a275edd0bca85f2b16e160c0d
Georgi <georgi.ginduzov17d@gmail.com>
2024-02-19 22:39:33 +0200

    Add Drone class with constructor

---
+++ Scripts/Classes/Drone.js
@@ -0,0 +1,12 @@
+class Drone {
+    constructor(currentLocation) {
+      this.currentLocation = currentLocation;
+      this.destination = null;
+      this.status = 'idle';
+    }
+
+    calculateTimeToDestination(destination) {
+    console.log(destination);
+    }
+}
+
