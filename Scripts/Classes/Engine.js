896dddb4917994b16e4371b72edd9dca50cce1ff
Georgi <georgi.ginduzov17d@gmail.com>
2024-02-20 00:14:03 +0200

    Add customer and order functionality to Engine class

--- Scripts/Classes/Engine.js
+++ Scripts/Classes/Engine.js
@@ -20,13 +20,30 @@ class Engine{
         for (let warehouse of data['warehouses']) {
             map.addWarehouse(warehouse['x'], warehouse['y'], warehouse['name'])
         }
+
+        for (let customer of data['customers']) {
+            map.addCustomer(customer['id'], customer['name'], customer['coordinates'])
+        }
+
+        /*for (let order of data['orders']) {
+            map.addOrder(order['id'])
+        }*/
+
+        for (let orderData of data['orders']) {
+            let productList = Object.entries(orderData.productList).map(([name, count]) => ({name, count}));
+            let order = new Order(orderData.customerId, productList);
+            // To Do - Calculation of the correct warehouse for the order
+            // Add the order to the appropriate warehouse
+          }
+
         return map;
     }
 
     start(){
-        const twoDImentionalMap = this.initializeMap('C:/Users/Asus/source/GitLab repos/georgi-ginduzov-nemetschek-rise-2024/Scripts/Classes/Tests/BandASImlpifiedInputExample.json');
+        const twoDimentionalMap = this.initializeMap('C:/Users/Asus/source/GitLab repos/georgi-ginduzov-nemetschek-rise-2024/Scripts/Classes/Tests/BandASImlpifiedInputExample.json');
 
-        console.log(twoDImentionalMap);
+        //console.log(JSON.stringify(twoDimentionalMap, null, 2));
+        console.log(twoDimentionalMap);
         // To do - implement other attributes value assignment
         // To do - implement movement logic
 
