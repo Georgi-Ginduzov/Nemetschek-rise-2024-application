c1ff7e3d005049812166a9ae6a8918a39f166836
Georgi <georgi.ginduzov17d@gmail.com>
2024-02-20 00:15:38 +0200

    Add Customer class and method to TwoDimensionalMap

--- Scripts/Classes/TwoDimensionalMap.js
+++ Scripts/Classes/TwoDimensionalMap.js
@@ -1,14 +1,20 @@
 const Warehouse = require('./Warehouse.js');
+const Customer = require('./Customer.js');
+
 class TwoDimensionalMap{
     constructor(coordinates){
         this.coordinates = coordinates;
         this.warehouses = [];
+        this.customers = [];
     }
 
     addWarehouse(x, y, name){
         this.warehouses.push(new Warehouse(x, y, name));
     }
 
+    addCustomer(id, name, coordinates){
+        this.customers.push(new Customer(id, name, coordinates));
+    }
 }
 
 module.exports = TwoDimensionalMap;
