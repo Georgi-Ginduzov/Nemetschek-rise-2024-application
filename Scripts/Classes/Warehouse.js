f845fc43274eaa08c191828191fefdcd7e9b73c1
Georgi <georgi.ginduzov17d@gmail.com>
2024-02-21 00:55:57 +0200

    Added get order method

--- Scripts/Classes/Warehouse.js
+++ Scripts/Classes/Warehouse.js
@@ -7,10 +7,15 @@ class Warehouse {
         this.name = name;
         this.products = [];
         this.orders = [];
+        //this.drone = drone;
     }
 
     addOrder(orderId, productList) {
         this.orders.push(new Order(orderId, productList));
+    }
+
+    getOrder(orderNumber){
+        return this.orders[orderNumber];
     }
 }
 
